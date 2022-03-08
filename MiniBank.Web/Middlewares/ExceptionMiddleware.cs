b638cf5e2cda5a09fe45b42abf8c992f58f9285e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-08 23:45:38 +0300

    feat: realise Middlewares for exception handling
    - Realise UserFriendlyExceptionMiddleware for handling UserFriendlyException
    - Realise ExceptionMiddleware for handling all the other exceptions

---
+++ MiniBank.Web/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MiniBank.Web.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsJsonAsync(new { Error = "Internal error!" });
+            }
+        }
+    }
+}
---
+++ MiniBank.Web/Middlewares/UserFriendlyExceptionMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using MiniBank.Core.Exceptions;
+
+namespace MiniBank.Web.Middlewares
+{
+    public class UserFriendlyExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public UserFriendlyExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (UserFriendlyException userFriendlyException)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsJsonAsync(new { Error = $"{userFriendlyException.Message}" });
+            }
+        }
+    }
+}
