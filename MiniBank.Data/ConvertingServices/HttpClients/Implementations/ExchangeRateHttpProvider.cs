182e1922b0569e7cbd2c73f757c52388680c46e7
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-16 23:39:03 +0300

    feat: improve ExchangeRateProvider
    - Now GetCourse method makes http request to get a course

---
+++ MiniBank.Data/ConvertingServices/HttpClients/Implementations/ExchangeRateHttpProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using MiniBank.Core.Domains.CurrencyConverting.Services;
+using MiniBank.Core.Exceptions;
+using MiniBank.Data.ConvertingServices.HttpClients.Models;
+
+namespace MiniBank.Data.ConvertingServices.HttpClients.Implementations
+{
+    public class ExchangeRateHttpProvider : IExchangeRateProvider
+    {
+        private readonly HttpClient _httpClient;
+        
+        public ExchangeRateHttpProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        
+        public double GetCourse(string currencyCode)
+        {
+            var response = _httpClient.GetFromJsonAsync<ExchangeRateResponse>("daily_json.js")
+                .GetAwaiter().GetResult();
+
+            if (!response.Currencies.ContainsKey(currencyCode))
+            {
+                throw new ValidationException($"{currencyCode} is not correct currency code!");
+            }
+            
+            return response.Currencies[currencyCode].Value;
+        }
+    }
+}
