a03188bbc8047b986ea0962c20841dac1540000a
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-29 23:58:53 +0300

    feat: realise MiniBankContext

---
+++ MiniBank.Data/MiniBankContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MiniBank.Data.BankAccounts;
+using MiniBank.Data.Transactions;
+using MiniBank.Data.Users;
+
+namespace MiniBank.Data
+{
+    public class MiniBankContext : DbContext
+    {
+        public DbSet<UserDbModel> Users { get; set; }
+        
+        public DbSet<BankAccountDbModel> BankAccounts { get; set; }
+        
+        public DbSet<TransactionDbModel> Transactions { get; set; }
+
+        public MiniBankContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MiniBankContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+    
+    public class Factory : IDesignTimeDbContextFactory<MiniBankContext>
+    {
+        public MiniBankContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseNpgsql("Host=localhost;Port=5432;Database=MiniBank;Username=postgres;Password=777")
+                .Options;
+
+            return new MiniBankContext(options);
+        }
+    }
+}
