f9adb66118244628535215b1a4c186fc8d692ba8
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-04-04 14:18:58 +0300

    feat: add usage of context to Users
    - Add Map configuration to UserDbModel
    - Remove List<UserDbModel> from UserRepository
      and add usage of MiniBankContext

--- MiniBank.Data/Users/Repositories/UserRepository.cs
+++ MiniBank.Data/Users/Repositories/UserRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using MiniBank.Core.Domains.Users;
 using MiniBank.Core.Domains.Users.Repositories;
 using MiniBank.Data.Exceptions;
@@ -9,11 +11,16 @@ namespace MiniBank.Data.Users.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        private readonly List<UserDbModel> _users = new();
+        private readonly MiniBankContext _context;
+
+        public UserRepository(MiniBankContext context)
+        {
+            _context = context;
+        }
 
         public void Add(User user)
         {
-            _users.Add(new UserDbModel
+            _context.Users.Add(new UserDbModel
             {
                 Id = user.Id,
                 Login = user.Login,
@@ -23,7 +30,9 @@ namespace MiniBank.Data.Users.Repositories
 
         public User GetById(Guid id)
         {
-            var dbModel = _users.FirstOrDefault(currentUser => currentUser.Id == id);
+            var dbModel = _context.Users
+                .AsNoTracking()
+                .FirstOrDefault(currentUser => currentUser.Id == id);
             if (dbModel == null)
             {
                 throw new ObjectNotFoundException($"User with id: {id} is not found!");
@@ -39,7 +48,9 @@ namespace MiniBank.Data.Users.Repositories
 
         public IEnumerable<User> GetAll()
         {
-            return _users.Select(user => new User
+            return _context.Users
+                .AsNoTracking()
+                .Select(user => new User
             {
                 Id = user.Id,
                 Login = user.Login,
@@ -49,7 +60,7 @@ namespace MiniBank.Data.Users.Repositories
 
         public void Update(User user)
         {
-            var dbModel = _users.FirstOrDefault(currentUser => currentUser.Id == user.Id);
+            var dbModel = _context.Users.FirstOrDefault(currentUser => currentUser.Id == user.Id);
             if (dbModel == null)
             {
                 throw new ObjectNotFoundException($"User with id: {user.Id} is not found!");
@@ -61,19 +72,19 @@ namespace MiniBank.Data.Users.Repositories
 
         public void DeleteById(Guid id)
         {
-            _users.RemoveAt(GetIndex(id));
+            _context.Users.Remove(FindUser(id));
         }
 
         public bool Exists(Guid id)
         {
-            var index = GetIndex(id);
-            return index != -1;
+            var dbModel = FindUser(id);
+            return dbModel != null;
         }
 
-        private int GetIndex(Guid id)
+        private UserDbModel FindUser(Guid id)
         {
-            var index = _users.FindIndex(currentUser => currentUser.Id == id);
-            return index;
+            var dbModel = _context.Users.Find(id);
+            return dbModel;
         }
     }
 }
--- MiniBank.Data/Users/UserDbModel.cs
+++ MiniBank.Data/Users/UserDbModel.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MiniBank.Core.Domains.BankAccounts;
+using MiniBank.Data.BankAccounts;
 
 namespace MiniBank.Data.Users
 {
@@ -9,5 +14,18 @@ namespace MiniBank.Data.Users
         public string Login { get; set; }
 
         public string Email { get; set; }
+        
+        public virtual List<BankAccountDbModel> BankAccounts { get; set; }
+
+        internal class Map : IEntityTypeConfiguration<UserDbModel>
+        {
+            public void Configure(EntityTypeBuilder<UserDbModel> builder)
+            {
+                builder.HasKey(dbModel => dbModel.Id).HasName("pk_user");
+                builder.Property(dbModel => dbModel.Id).HasColumnName("id");
+                builder.Property(dbModel => dbModel.Login).HasColumnName("login");
+                builder.Property(dbModel => dbModel.Email).HasColumnName("email");
+            }
+        }
     }
 }
