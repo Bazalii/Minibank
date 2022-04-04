f45e0c9eb9ee6decf7d95ef48efd29b6425c7dba
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-04-04 14:23:24 +0300

    feat: realise new exception

---
+++ MiniBank.Web/Exceptions/ServiceNotRegisteredException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniBank.Web.Exceptions
+{
+    public class ServiceNotRegisteredException : Exception
+    {
+        public ServiceNotRegisteredException(string message)
+            : base(message)
+        {
+        }
+    }
+}
