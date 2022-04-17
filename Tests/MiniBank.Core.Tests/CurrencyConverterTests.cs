db15959467cbc5aa1789135ec14227cce4be7fcc
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-04-17 19:31:09 +0300

    feat: realise CurrencyConverterTests

---
+++ Tests/MiniBank.Core.Tests/CurrencyConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using MiniBank.Core.Domains.CurrencyConverting.Services;
+using MiniBank.Core.Domains.CurrencyConverting.Services.Implementations;
+using MiniBank.Core.Enums;
+using MiniBank.Core.Exceptions;
+using Moq;
+using Xunit;
+
+namespace MiniBank.Core.Tests;
+
+public class CurrencyConverterTests
+{
+    private readonly ICurrencyConverter _mockCurrencyConverter;
+
+    private readonly Mock<IExchangeRateProvider> _mockExchangeRateProvider;
+
+    public CurrencyConverterTests()
+    {
+        _mockExchangeRateProvider = new Mock<IExchangeRateProvider>();
+        _mockExchangeRateProvider
+            .Setup(provider => provider.GetCourse("RUB"))
+            .ReturnsAsync(1);
+        _mockExchangeRateProvider
+            .Setup(provider => provider.GetCourse("EUR"))
+            .ReturnsAsync(80);
+        _mockExchangeRateProvider
+            .Setup(provider => provider.GetCourse("USD"))
+            .ReturnsAsync(70);
+
+        _mockCurrencyConverter = new CurrencyConverter(_mockExchangeRateProvider.Object);
+    }
+
+    [Fact]
+    public async Task ConvertCurrency_AmountIsNegative_ThrowException()
+    {
+        var exception = await Assert.ThrowsAsync<ValidationException>(() =>
+            _mockCurrencyConverter.ConvertCurrency(-1, Currencies.EUR, Currencies.RUB));
+
+        Assert.Contains("The amount of money cannot be negative!", exception.Message);
+    }
+
+    [Theory]
+    [InlineData(1, Currencies.EUR, Currencies.RUB, 80)]
+    [InlineData(1, Currencies.RUB, Currencies.RUB, 1)]
+    [InlineData(2.35, Currencies.EUR, Currencies.RUB, 188)]
+    [InlineData(0.57, Currencies.USD, Currencies.EUR, 0.5)]
+    public async Task ConvertCurrency_SuccessPath_ReturnsConvertedResultGetCourseIsCalledTwice(double amount,
+        Currencies fromCurrency, Currencies toCurrency, double expectedResult)
+    {
+        var result = await _mockCurrencyConverter.ConvertCurrency(amount, fromCurrency, toCurrency);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
