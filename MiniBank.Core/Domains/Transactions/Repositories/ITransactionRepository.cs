ae16dddf65a2d841f4a876d8d49d3113b3dc19ee
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-04-19 16:34:29 +0300

    feat: improve TransactionRepository
    - Add usage of cancellation tokens

--- MiniBank.Core/Domains/Transactions/Repositories/ITransactionRepository.cs
+++ MiniBank.Core/Domains/Transactions/Repositories/ITransactionRepository.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MiniBank.Core.Domains.Transactions.Repositories
 {
     public interface ITransactionRepository
     {
-        Task Add(Transaction transaction);
+        Task Add(Transaction transaction, CancellationToken cancellationToken);
 
-        Task<Transaction> GetById(Guid id);
+        Task<Transaction> GetById(Guid id, CancellationToken cancellationToken);
 
-        Task Update(Transaction transaction);
+        Task Update(Transaction transaction, CancellationToken cancellationToken);
 
-        Task DeleteById(Guid id);
+        Task DeleteById(Guid id, CancellationToken cancellationToken);
     }
 }
