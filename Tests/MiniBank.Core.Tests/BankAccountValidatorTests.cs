64beacac4a85571d0248c84b9ec39d08687776d0
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-04-19 16:50:25 +0300

    feat: realise BankAccountValidatorTests

---
+++ Tests/MiniBank.Core.Tests/BankAccountValidatorTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentValidation;
+using MiniBank.Core.Domains.BankAccounts;
+using MiniBank.Core.Domains.BankAccounts.Validators;
+using MiniBank.Core.Domains.Users.Repositories;
+using MiniBank.Core.Enums;
+using Moq;
+using Xunit;
+
+namespace MiniBank.Core.Tests;
+
+public class BankAccountValidatorTests
+{
+    private readonly AbstractValidator<BankAccount> _bankAccountValidator;
+
+    private readonly Mock<IUserRepository> _mockUserRepository;
+
+    public BankAccountValidatorTests()
+    {
+        _mockUserRepository = new Mock<IUserRepository>();
+
+        _bankAccountValidator = new BankAccountValidator(_mockUserRepository.Object);
+    }
+
+    [Fact]
+    public async Task ValidateAndThrowAsync_AccountNegativeAmountOfMoney_ThrowException()
+    {
+        var userId = Guid.NewGuid();
+
+        var bankAccount = new BankAccount
+        {
+            UserId = userId,
+            AmountOfMoney = -100
+        };
+
+        _mockUserRepository
+            .Setup(repository => repository.Exists(userId, default))
+            .ReturnsAsync(false);
+
+        var exception =
+            await Assert.ThrowsAsync<ValidationException>(() =>
+                _bankAccountValidator.ValidateAndThrowAsync(bankAccount));
+
+        Assert.Contains("AmountOfMoney: should be greater than 0!", exception.Message);
+    }
+
+    [Fact]
+    public async Task ValidateAndThrowAsync_AccountWithUserThatNotExists_ThrowException()
+    {
+        var userId = Guid.NewGuid();
+
+        var bankAccount = new BankAccount
+        {
+            UserId = userId,
+            AmountOfMoney = 100
+        };
+
+        _mockUserRepository
+            .Setup(repository => repository.Exists(userId, default))
+            .ReturnsAsync(false);
+
+        var exception =
+            await Assert.ThrowsAsync<ValidationException>(() =>
+                _bankAccountValidator.ValidateAndThrowAsync(bankAccount));
+
+        Assert.Contains("UserId: user with entered Id doesn't exist!", exception.Message);
+    }
+
+    [Fact]
+    public async Task ValidateAndTrowAsync_SuccessPath_UserIsValidated()
+    {
+        var userId = Guid.NewGuid();
+
+        var bankAccount = new BankAccount
+        {
+            UserId = userId,
+            AmountOfMoney = 100,
+            CurrencyCode = Currencies.EUR
+        };
+
+        _mockUserRepository
+            .Setup(repository => repository.Exists(userId, default))
+            .ReturnsAsync(true);
+
+        await _bankAccountValidator.ValidateAndThrowAsync(bankAccount);
+
+        _mockUserRepository
+            .Verify(
+                repository => repository.Exists(bankAccount.UserId, default),
+                Times.Once());
+    }
+}
