a322eb64cc86ce1b68f9f6bb16d5393fb48d006b
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-05-01 17:46:27 +0300

    feat: add authentication with tokens
    - Add [Authorize] annotation to controllers
    - Add authentication to swagger
    - Add special middleware
    - Move corresponding links to configuration

--- MiniBank.Web/Startup.cs
+++ MiniBank.Web/Startup.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Models;
 using MiniBank.Core;
 using MiniBank.Data;
@@ -22,11 +27,58 @@ namespace MiniBank.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            services.AddSwaggerGen(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "MiniBank.Web", Version = "v1"});
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniBank.Web", Version = "v1" });
+
+                options.AddSecurityDefinition("oauth2",
+                    new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.OAuth2,
+                        Flows = new OpenApiOAuthFlows
+                        {
+                            ClientCredentials = new OpenApiOAuthFlow
+                            {
+                                TokenUrl = new Uri(Configuration["TokenUri"]),
+                                Scopes = new Dictionary<string, string>()
+                            }
+                        }
+                    });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = SecuritySchemeType.OAuth2.GetDisplayName(),
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
             });
-            services.AddCore().AddData(Configuration).AddWeb();
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Audience = "api";
+                    options.Authority = Configuration["AuthorityUri"];
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateLifetime = true,
+                        ValidateAudience = false
+                    };
+                });
+
+            services
+                .AddCore()
+                .AddData(Configuration)
+                .AddWeb();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -44,6 +96,8 @@ namespace MiniBank.Web
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
